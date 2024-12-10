892f20dc7c2b9fcbe97a90933ca11ec188db9313
ocalalex <157875326+ocalalex@users.noreply.github.com>
2024-12-09 20:25:14 -0500

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -69,7 +69,7 @@ if mode != 'subscrape':
     # Function that runs bash script scraper using subprocess.run()
     def bash_scrape():
       print("Current working directory:", os.getcwd())
-        print("Directory contents:", os.listdir(os.getcwd()))
+      print("Directory contents:", os.listdir(os.getcwd()))
       command = f"src/main/clone_scraper.sh ./clone_urls.txt {export_bash_csv} {remove}"
       proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
